4c828803463620f61333d38e1c07a20bf3acd27a
DragosRomaniucStint <dragos.romaniuc@stint.co>
2022-01-27 15:42:48 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { BakeShadows, OrbitControls, Stage } from '@react-three/drei'
+import Car from './Car'
+
+
+export default function Viewer() {
+  return (
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 150], fov: 40 }}>
+      <Suspense fallback={null}>
+        <Stage environment="city" intensity={0.5} contactShadow={{ opacity: 0.7, blur: 2 }}>
+          <Car color="tomato" position={[0, -1, 0]} />
+          <Car color="orange" scale={-1} rotation={[0, 0.5, Math.PI]} position={[0, -1, -2]} />
+        </Stage>
+        <BakeShadows />
+      </Suspense>
+      <OrbitControls autoRotate />
+    </Canvas>
+  )
+}
